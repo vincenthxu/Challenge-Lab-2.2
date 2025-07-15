1744e7190931c3b0d7e70f14667e42c4eb171fbb
Vincent Xu <vincent.xu1@outlook.com>
2025-07-15 09:52:31 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Challenge_Lab_2._2
+{
+    internal class Program
+    {
+        static private int attempts = 0;
+        static Dictionary<string, string> db = new()
+        {
+            { "admin", "admin" },
+            { "root", "root" },
+            { "user", "password" },
+        };
+        static void Main(string[] args)
+        {
+            String? user = null;
+            String? pass = null;
+            while(attempts < 3)
+            {
+                while (user == null)
+                {
+                    Console.Write("username: ");
+                    user = Console.ReadLine();
+                }
+
+                Console.Write("password: ");
+                pass = Console.ReadLine();
+
+                Console.Clear();
+
+                if (db.ContainsKey(user) && db[user] == pass)
+                {
+                    Console.WriteLine("Login successful");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid password");
+                    user = null;
+                    pass = null;
+                    attempts++;
+                }
+            }
+            if (attempts == 3)
+            {
+                Console.WriteLine("Too many invalid attempts");
+            }
+        }
+    }
+}
